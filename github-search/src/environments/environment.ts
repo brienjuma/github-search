4cee24da697425127df01fabf4073ed63558a94f
jumaBrian <njengabrianj@gmail.com>
2022-04-20 12:55:26 +0300

    refactor: Place access key

--- github-search/src/environments/environment.ts
+++ github-search/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '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'
 };
 
 /*
