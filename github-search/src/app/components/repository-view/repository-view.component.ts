f9f1c5b278026084a44fcf028e6e142af447481c
jumaBrian <njengabrianj@gmail.com>
2022-04-21 02:03:43 +0300

    success: Implement Repository search

--- github-search/src/app/components/repository-view/repository-view.component.ts
+++ github-search/src/app/components/repository-view/repository-view.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { RepoSearchService } from 'src/app/services/repo-search/repo-search.service';
+import { RepositorySearch } from 'src/app/models/repository-search/repository-search';
 
 @Component({
   selector: 'app-repository-view',
   templateUrl: './repository-view.component.html',
-  styleUrls: ['./repository-view.component.css']
+  styleUrls: ['./repository-view.component.css'],
+  providers: [ RepoSearchService]
 })
 export class RepositoryViewComponent implements OnInit {
 
-  constructor() { }
+  repositories!: RepositorySearch;
+
+  constructor(public repoSearch: RepoSearchService) { }
 
   ngOnInit(): void {
+    this.repositories = this.repoSearch.repositories;
+    this.searchRepo("akan-names");
+
   }
 
+  searchRepo(searchName: string){
+    if (searchName !=='') {
+      this.repoSearch.getRepos(searchName)
+      console.log(searchName)
+    };
+  };
 }
