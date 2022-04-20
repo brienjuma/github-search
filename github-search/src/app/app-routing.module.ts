9ad1e0af81c52df4a1382acb8576a0e4f508dde0
jumaBrian <njengabrianj@gmail.com>
2022-04-20 21:57:07 +0300

    refactor: Resolve App-routing

--- github-search/src/app/app-routing.module.ts
+++ github-search/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvalidSearchComponent } from './components/invalid-search/invalid-search.component';
+import { RepositoryViewComponent } from './components/repository-view/repository-view.component';
+import { SearchComponent } from './components/search/search.component';
+import { UserBioInformationComponent } from './components/user-bio-information/user-bio-information.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user-search', component: SearchComponent},
+  {path: 'repository-search', component: RepositoryViewComponent},
+  {path: '',redirectTo: '/user-search', pathMatch: "full"},
+  {path: '**', component: InvalidSearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
